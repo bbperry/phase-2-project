d53bd1e6be1d57e808b61c29dfcc4ce225a7f1eb
Ben <benbperry1@gmail.com>
2021-12-07 14:49:36 -0800

    added bensbranch

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -1,16 +1,34 @@
 import React from "react"
+import { Select } from 'semantic-ui-react'
+
+
+const categoryOptions = [
+  { key: 'af', value: 'af', text: 'Phonology & Articulation' },
+  { key: 'ax', value: 'ax', text: 'Concepts & Prepositions' },
+  { key: 'al', value: 'al', text: 'Albania' },
+  { key: 'dz', value: 'dz', text: 'Algeria' },
+  { key: 'as', value: 'as', text: 'American Samoa' },
+  { key: 'ad', value: 'ad', text: 'Andorra' },
+  { key: 'ao', value: 'ao', text: 'Angola' },
+  { key: 'ai', value: 'ai', text: 'Anguilla' },
+  { key: 'ag', value: 'ag', text: 'Antigua' },
+  { key: 'ar', value: 'ar', text: 'Argentina' },
+] 
+
 
 function FilterBar({ setBookSearch, books }) {
   return (
-    <>
-      
+  
+ <div>   
 
       <input
         type="text"
         placeholder="Search..."
         onChange={(e) => setBookSearch(e.target.value)}
       />
-    </>
+<Select placeholder='Select your Category' options={categoryOptions} />
+
+    </div>
   );
 }
 
